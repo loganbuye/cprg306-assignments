7b9e4e6d70b557f4c2666b3455e8968239183b38
Logan <logan.buye@edu.sait.ca>
2024-09-12 13:02:55 -0600

    Week 2 Assignment Code
    I completed the week 2 assignment page code and created the student-info component. I also added a heading and link to the week 2 page to the root page.

--- app/page.js
+++ app/page.js
@@ -1,9 +1,11 @@
+import Link from "next/link";
 
 
 export default function Home() {
   return (
     <main>
-
+      <h1>CPRG 306: Web Development 2 - Assignments</h1>
+      <Link href="./week-2/">Week 2</Link>
     </main>
   );
 }
---
+++ app/week-2/student-info.js
@@ -0,0 +1,11 @@
+import Link from "next/link";
+
+
+export default function StudentInfo() {
+    return (
+        <main>
+            <h1>Logan Buye</h1>
+            <Link href="https://github.com/loganbuye/cprg306-assignments">GitHub Repository</Link>
+        </main>
+    );
+}
